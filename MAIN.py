37ece3133bf4c4752454219c5fa67b5ab29b66d3
Kuhtal <leonidpetrovsky@gmail.com>
2023-05-23 18:21:18 +0300

    adding game objects

--- MAIN.py
+++ MAIN.py
@@ -6,6 +6,15 @@ H = 720
 FPS = 1000
 clock = pg.time.Clock()
 
+GREY = (230, 230,230)
+WHITE = (255,255,255)
+VIOLET = (230,61,245)
+
+
+
+player = pg.Rect(W - 20, H //2,10,150)
+opponent = pg.Rect(10, H //2,10,150)
+ball = pg.Rect(W // -15, H // -15,30,30)
 pg.init()  # инициализируем pygame
 screen = pg.display.set_mode((W, H))
 pg.display.set_caption('Ping Pong | ') # создаем экран игры разрешением 1280х720px
@@ -15,3 +24,10 @@ while True:  # цикл игры
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+
+    screen.fiil(GREY)
+    pg.draw.rect(screen, VIOLET, player)
+    pg.draw.rect(screen, VIOLET, opponent)
+    pg.draw.aaline(screen, WHITE, [W // 2, 0], [W // 2, H])
+    pg.draw.ellipse(screen, VIOLET, ball)
+    pg.display.update()
